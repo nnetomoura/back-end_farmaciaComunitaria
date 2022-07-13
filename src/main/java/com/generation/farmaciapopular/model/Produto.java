e2091413aa11d1fd9ba72d62b94ece5f5ce4288b
Claudio Pereira <claudio.prepara0@gmail.com>
2022-07-13 14:55:19 -0300

    atualização das tabelas de produto e categoria

--- src/main/java/com/generation/farmaciapopular/model/Produto.java
+++ src/main/java/com/generation/farmaciapopular/model/Produto.java
@@ -28,28 +28,92 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank (message = "O nome do medicamento é Obrigatório!")
-	@Size (min = 5, max = 100, message = "O nome do medicamento deve conter no mínimo 5 e no máximo 100 caracteres. ")
+	@NotBlank(message = "O nome do medicamento é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O nome do medicamento deve conter no mínimo 5 e no máximo 100 caracteres. ")
 	private String nomeProduto;
 	
-	@Size (max = 1000, message="A descrição do produto deve conter no máximo 1000(um mil) caracteres.")
+	@Size(max = 1000, message="A descrição do produto deve conter no máximo 1000(um mil) caracteres.")
 	private String descricaoProduto;
 	
-	@NotBlank (message = "O preço do medicamento é obrigatório!")
+	@NotBlank(message = "O preço do medicamento é obrigatório!")
 	private DecimalFormat valorProduto;
 	
-	@Size (max = 2000)
+	@Size(max = 2000)
 	private String fotoProduto;
 	
-	@Size (min = 1, max = 500)
+	@Size(min = 1, max = 500)
 	private String estoqueProduto;
 	
 	@UpdateTimestamp
 	private LocalDateTime dataCadastro;
 
 	@ManyToOne
-	@JsonIgnoreProperties ("produto")
+	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public DecimalFormat getValorProduto() {
+		return valorProduto;
+	}
+
+	public void setValorProduto(DecimalFormat valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+
+	public String getFotoProduto() {
+		return fotoProduto;
+	}
+
+	public void setFotoProduto(String fotoProduto) {
+		this.fotoProduto = fotoProduto;
+	}
+
+	public String getEstoqueProduto() {
+		return estoqueProduto;
+	}
+
+	public void setEstoqueProduto(String estoqueProduto) {
+		this.estoqueProduto = estoqueProduto;
+	}
+
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 	
 	
