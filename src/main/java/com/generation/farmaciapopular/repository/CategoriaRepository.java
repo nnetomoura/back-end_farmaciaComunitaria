51658f9507492d23258ccaa5307ddf73853fbcd1
Claudio Pereira <claudio.prepara0@gmail.com>
2022-07-15 20:49:06 -0300

    criação dos repositórios(interfaces) de produto e categoria

---
+++ src/main/java/com/generation/farmaciapopular/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.farmaciapopular.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.farmaciapopular.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+	
+	public List<Categoria> findAllByNomeCategoriaContainingIgnoreCase(@Param("nome") String nome);
+
+}
