5ad165ac326ba234fb258fd8247eede5f154de77
Claudio Pereira <claudio.prepara0@gmail.com>
2022-07-21 22:18:47 -0300

    adicionados os comentários sobre a atualização javadoc contendo a anotação @param

--- src/main/java/com/generation/farmaciapopular/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmaciapopular/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.generation.farmaciapopular.repository;
 
+/*Observação: a anotação @param é recente. Basicamente é um tipo especial de commit utilizado pelo javadoc
+e descreverá o parâmetro que um método pode receber. Nesse caso, o método de busca pelo nome do produto
+deverá ser preenchido especificamente com um "nome" sendo um objeto do tipo String. Foi inserida 
+para solucionar possíveis problemas de busca nos parâmetros em geral, onde a especificação do seu tipo
+tal questão*/
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +15,8 @@ import com.generation.farmaciapopular.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
+	//forma usada anteriormente: 
+	/*public List <Produto> findAllByNomeProdutoContainingIgnoreCase(String nome);*/
 	public List <Produto> findAllByNomeProdutoContainingIgnoreCase(@Param("nome") String nome);
 
 }
