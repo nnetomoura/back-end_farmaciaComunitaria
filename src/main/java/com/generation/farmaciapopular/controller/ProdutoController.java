d51210b22a79c8669247018978d19ae458e90563
Claudio Pereira <claudio.prepara0@gmail.com>
2022-07-29 15:33:47 -0300

    finalização da camada controller de produto com CRUD geral completo (métodos get, put, post e delete)

--- src/main/java/com/generation/farmaciapopular/controller/ProdutoController.java
+++ src/main/java/com/generation/farmaciapopular/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.generation.farmaciapopular.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,12 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.farmaciapopular.model.Produto;
 import com.generation.farmaciapopular.repository.CategoriaRepository;
@@ -49,5 +54,28 @@ public class ProdutoController {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+		if (produtoRepository.existsById(produto.getId())) {
+			if (categoriaRepository.existsById(produto.getCategoria().getId()))
+				return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+	}
 
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+	
+	if (produto.isEmpty())
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	
+	produtoRepository.deleteById(id);
+	
+	
+	}
 }
